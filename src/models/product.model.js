47d24b738038d9d17dae2227889d5cd59b31f5d8
jobpawat <pawat-ka@rmutp.ac.th>
2021-02-28 19:13:38 +0700

    chage sql in edit product

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -7,8 +7,28 @@ class ProductModel {
 
     find = async (params = {}) => {
         const keys = Object.keys(params)
-        let sql = `SELECT products.pid, products.sid, products.name, products.description, products.url, 
-                products.price, products.type_id, stock.remain as stock FROM ${this.table} INNER JOIN stock stock ON products.stock_id = stock.stock_id`
+        let sql = `
+        SELECT products.pid, 
+                products.sid, 
+                products.name, 
+                products.description, 
+                products.url, 
+                products.status, 
+                products.price, 
+                products.type_id, 
+                stock.remain as stock ,
+                shops.name as shop_name,
+                shops.status as shop_status,
+                product_type.name as type_name
+                FROM ${this.table} 
+                INNER JOIN stock
+                ON products.stock_id = stock.stock_id
+                INNER JOIN shops
+                ON products.sid = shops.sid
+                INNER JOIN product_type
+                ON products.type_id = product_type.type_id
+                `
+
         if (keys.length == 0)
             return await query(sql)
         const { columns, values } = pairSQL(params)
@@ -48,10 +68,22 @@ class ProductModel {
 
         //update products data
         delete params['stock'];
-        const { columns, values } = pairSQL(params)
+        /*const { columns, values } = pairSQL(params)
         const sql = `UPDATE ${this.table} SET ${columns} WHERE pid = ?`
-
-        const result = await query(sql, [...values, id])
+        */
+        
+        //edit sql
+        const sql = `
+            UPDATE ${this.table} SET 
+                type_id = ${params['type_id']}, 
+                name = '${params['name']}',
+                description = '${params['description']}',
+                url = '${params['url']}',
+                price = ${params['price']},
+                status = '${params['status']}'
+                WHERE pid = '${params['pid']}'
+                `;
+        const result = await query(sql)
         return result
     }
 
